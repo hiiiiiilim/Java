a17cd6b2aa77ecef23de3c804905d56525639d11
hiiiiiilim <vnfwlq07@naver.com>
2023-09-12 17:28:32 +0900

    .

---
+++ 07_objectArray/src/com/kh/practice/student/controller/StudentController.java
@@ -0,0 +1,15 @@
+package com.kh.practice.student.controller;
+
+import com.kh.practice.student.model.vo.Student;
+
+public class StudentController {
+	Student[] sArr = new Student[5];
+	int CUT_LINE = 60;
+	
+	public StudentController() {
+		for(Student arr: sArr) {
+			
+		}
+	}
+
+}
