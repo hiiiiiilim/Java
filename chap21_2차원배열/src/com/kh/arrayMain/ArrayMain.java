0914fd41c63bc2ee01baaedf3e45b5d4509aaa69
hiiiiiilim <vnfwlq07@naver.com>
2023-09-12 17:30:46 +0900

    2차원배열

---
+++ chap21_2차원배열/src/com/kh/arrayMain/ArrayMain.java
@@ -0,0 +1,14 @@
+package com.kh.arrayMain;
+
+import com.kh.arrayexam.ArrayExam;
+
+public class ArrayMain {
+	public static void main(String[] args) {
+		ArrayExam practice = new ArrayExam();
+		
+		//practice.practice1();
+		//practice.practice2();
+		//practice.practice3();
+		practice.practice4();
+	}
+}
